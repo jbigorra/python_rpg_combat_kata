661f72116ef4a8e08b109446ab8b3faa734a9ef8
Juan bigorra <juanchoibf@gmail.com>
2022-09-18 17:15:49 +0200

    Implement heal cases

--- src/character.py
+++ src/character.py
@@ -6,5 +6,14 @@ class Character:
     def is_alive(self):
         return self.health > 0
 
-    def damage(self, character: "Character", damage: int):
-        character.health = character.health - damage
+    def damage(self, character: "Character", amount: int):
+        character.health -= amount
+
+    def heal(self, character: "Character", amount: int):
+        if not character.is_alive():
+            return
+
+        character.health += amount
+
+        if character.health > 1000:
+            character.health = 1000
