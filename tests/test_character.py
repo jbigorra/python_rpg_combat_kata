0abf6fcc0c909bf0b08d95e7f5ba3394f1b0d1bc
Juan bigorra <juanchoibf@gmail.com>
2022-09-19 21:46:39 +0200

    Move factory to dedicated folder and file

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,40 +1,7 @@
 import pytest
 
 from src.character import Character, CharacterType, CharacterConfig
-
-
-class CharacterFactory:
-    level: int = CharacterConfig.LEVEL
-    health: int = CharacterConfig.MAXIMUM_HEALTH
-    type: CharacterType = CharacterConfig.TYPE
-
-    def _reset_state(self):
-        self.level = CharacterConfig.LEVEL
-        self.health = CharacterConfig.MAXIMUM_HEALTH
-        self.type = CharacterConfig.TYPE
-
-    def with_level(self, level: int) -> "CharacterFactory":
-        self.level = level
-        return self
-
-    def with_health(self, health: int) -> "CharacterFactory":
-        self.health = health
-        return self
-
-    def with_type(self, type: CharacterType) -> "CharacterFactory":
-        self.type = type
-        return self
-
-    def build(self) -> "Character":
-        character = Character(level=self.level, health=self.health, type=self.type)
-        self._reset_state()
-        return character
-
-    def setup_base_ranged_character(self) -> "CharacterFactory":
-        self.level = CharacterConfig.LEVEL
-        self.health = CharacterConfig.MAXIMUM_HEALTH
-        self.type = CharacterType.RANGED
-        return self
+from tests.factories.character import CharacterFactory
 
 
 class TestCharacter:
